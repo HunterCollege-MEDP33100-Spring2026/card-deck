92b5e9da1f1fc5bd80456971ea138e42a4b1e968
Mamadou285 <mamadou.diallo81@login.cuny.edu>
2026-03-26 06:23:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,24 @@
-const deck = document.querySelector('.deck');
+// get the place where the cards will go
+const deck = document.getElementById("deck");
 
-/*
-    Step 1: Fetch data from your JSON file by:
-    - Inside of the getCards() function, use fetch() to load data.json
-    - Convert the response to JavaScript using .json()
-    - Call displayCards() with the data
-*/
+// get the data from data.json
+fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(item => {
+            // make one card
+            const card = document.createElement("div");
+            card.classList.add("card");
 
-function getCards() {
-    // Your code here for Step 1
-}
+            // put content inside the card
+            card.innerHTML = `
+        <img src="${item.image}" alt="${item.name}">
+        <h2>${item.name}</h2>
+        <p><strong>Type:</strong> ${item.type}</p>
+        <p>${item.fact}</p>
+      `;
 
-
-/*
-    Step 2: Display the cards
-    - Inside the for loop, loop through the array of card objects
-    - For each card, create HTML elements (using div, p, img, etc.)
-    - Append each card to the .deck container
-
-    Step 3: Add interactivity
-    - After creating the HTML elemetns, add at least one event listener to each card
-    - Some ideas: click to flip, highlight, show more info, etc.
-*/
-
-function displayCards(cards) {
-    for (let i = 0; i < cards.length; i++) {
-        // Your code here for Step 2 + 3
-    }
-}
-
-
-
-// DO NOT TOUCH (Starts loading the data)
-getCards();
+            // add card to the page
+            deck.appendChild(card);
+        });
+    });
